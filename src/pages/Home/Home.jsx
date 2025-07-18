d7e08f11f89d98fbfc0d976372f52f7e494a089e
Mikee Ojastro <mikeeojastro@gmail.com>
2025-07-18 20:39:06 +0800

    July 18, 2025

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,52 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Post from "../../components/Post/Post.jsx";
+
 function Home() {
+    const [userList, setUserList] = useState([]);
+
+    const getUsers = async () => {
+      // try-catch - statement used for error handling. Assuming that we always have an error. Executes a specific code block if "try" block fails
+      try {
+        const response = await axios.get("https://dummyjson.com/users");
+        // getting data from response
+        const { data } = response;
+        setUserList(data.users);
+      } catch (error) {
+        if(error) {
+          return alert("Cannot get users!");
+        }
+      }
+    }
+
+    // react hook that automatically runs as soon as the component is rendered
+    // runs if it detects a change in dependency
+    // useEffect(callback function, array of dependencies)
+    useEffect(() => {
+      if(userList.length === 0){
+        getUsers();
+      }
+    });
+
     return (
       <div className="home">
           <h1>Home</h1>
+          {
+            userList && 
+            userList.map((user) => {
+              const { id, firstName, lastName } = user;
+              return (
+                <div key={id}>
+                  <p>ID: {id}</p>
+                  <p>Name: {`${firstName} ${lastName}`}</p>
+                </div>
+              )
+            })
+          }
+          {
+            !userList && <div>No users available...</div>
+          }
+          <Post />
       </div>
     );
   }
